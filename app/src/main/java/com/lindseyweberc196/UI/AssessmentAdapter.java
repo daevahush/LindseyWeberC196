cecc9060e2bf6c906785c245679e8bd664d9947c
daevahush <daevahush@gmail.com>
2020-05-21 21:49:43 -0400

    Created adapters for Courses and Assessments

--- app/src/main/java/com/lindseyweberc196/UI/AssessmentAdapter.java
+++ app/src/main/java/com/lindseyweberc196/UI/AssessmentAdapter.java
@@ -1,4 +1,86 @@
 package com.lindseyweberc196.UI;
 
-public class AssessmentAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lindseyweberc196.Activity.TermsActivity;
+import com.lindseyweberc196.Entity.Assessment;
+import com.lindseyweberc196.Entity.Term;
+import com.lindseyweberc196.R;
+
+import java.util.List;
+
+public class AssessmentAdapter extends RecyclerView.Adapter<AssessmentAdapter.AssessmentViewHolder> {
+
+    class AssessmentViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView AssessmentItemView;
+
+        private AssessmentViewHolder(View itemView) {
+            super(itemView);
+            AssessmentItemView = itemView.findViewById(R.id.AssessmentTextView);
+            itemView.setOnClickListener((v) -> {
+                int position = getAdapterPosition();
+                final Assessment current = mAssessments.get(position);
+                Intent intent = new Intent(context, TermsActivity.class);
+                intent.putExtra("Assessment ID", current.getAssessmentID());
+                intent.putExtra("Assessment Type", current.getAssessmentType());
+                intent.putExtra("Assessment Name", current.getName());
+                intent.putExtra("Course ID", current.getCourseID());
+                intent.putExtra("Assessment Date", current.getDate());
+                intent.putExtra("Position", position);
+                context.startActivity(intent);
+            });
+        }
+    }
+
+
+
+    private final LayoutInflater mInflater;
+    private final Context context;
+    private List<Assessment> mAssessments;
+
+    public AssessmentAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @Override
+    public AssessmentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.assessment_list_item, parent, false);
+        return new AssessmentViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(AssessmentViewHolder holder, int position) {
+        if(mAssessments != null) {
+            Assessment current = mAssessments.get(position);
+            holder.AssessmentItemView.setText(current.getName());
+        } else {
+            holder.AssessmentItemView.setText("No Assessment");
+        }
+
+    }
+
+
+    public void setTerms(List<Assessment> assessments) {
+        mAssessments = assessments;
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public int getItemCount() {
+        if(mAssessments != null) {
+            return mAssessments.size();
+        } else {
+            return 0;
+        }
+    }
 }
--- app/src/main/java/com/lindseyweberc196/UI/CourseAdapter.java
+++ app/src/main/java/com/lindseyweberc196/UI/CourseAdapter.java
@@ -1,4 +1,87 @@
 package com.lindseyweberc196.UI;
 
-public class CourseAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.lindseyweberc196.Activity.CoursesActivity;
+import com.lindseyweberc196.Entity.Course;
+import com.lindseyweberc196.R;
+import java.util.List;
+
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder> {
+
+    class CourseViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView CourseItemView;
+
+        private CourseViewHolder(View itemView) {
+            super(itemView);
+            CourseItemView = itemView.findViewById(R.id.CourseTextView);
+            itemView.setOnClickListener((v) -> {
+                int position = getAdapterPosition();
+                final Course current = mCourses.get(position);
+                Intent intent = new Intent(context, CoursesActivity.class);
+                intent.putExtra("Course ID", current.getCourseID());
+                intent.putExtra("Term ID", current.getTermID());
+                intent.putExtra("Course Name", current.getTitle());
+                intent.putExtra("Status", current.getStatus());
+                intent.putExtra("Course Start Date", current.getStartDate());
+                intent.putExtra("Course End Date", current.getEndDate());
+                intent.putExtra("Mentor Name", current.getMentorName());
+                intent.putExtra("Mentor Phone", current.getMentorPhone());
+                intent.putExtra("Mentor Email", current.getMentorEmail());
+                intent.putExtra("Note", current.getNote());
+                intent.putExtra("Position", position);
+                context.startActivity(intent);
+            });
+        }
+    }
+
+
+
+    private final LayoutInflater mInflater;
+    private final Context context;
+    private List<Course> mCourses;
+
+    public CourseAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @Override
+    public CourseAdapter.CourseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.course_list_item, parent, false);
+        return new CourseAdapter.CourseViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(CourseAdapter.CourseViewHolder holder, int position) {
+        if(mCourses != null) {
+            Course current = mCourses.get(position);
+            holder.CourseItemView.setText(current.getTitle());
+        } else {
+            holder.CourseItemView.setText("No Course");
+        }
+
+    }
+
+
+    public void setCourses(List<Course> courses) {
+        mCourses = courses;
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public int getItemCount() {
+        if(mCourses != null) {
+            return mCourses.size();
+        } else {
+            return 0;
+        }
+    }
 }
